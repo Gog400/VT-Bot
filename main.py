0fe0d14a57e5e8fbdb9ac76834d1187fbf6c4632
Gog400 <gog400@mail.ru>
2019-07-29 00:20:56 +0300

    price recognizer

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ w, h = template.shape[::-1]
 
 def process_img(original_img):
     grey_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
-    (thresh, BnW_img) = cv2.threshold(grey_img, 127, 255, cv2.THRESH_BINARY)
 
-    # text = pytesseract.image_to_string(BnW_img, lang = 'eng')
 
     ## Нахождение предмета
     res = cv2.matchTemplate(grey_img, template, cv2.TM_CCOEFF_NORMED)
     loc = np.where( res >= 0.8)
 
     zoom_screen = original_img
+    BnW_img = original_img
     ## Обрисовка предмета
     for pt in zip(*loc[::-1]):
         cv2.rectangle(original_img, pt, (pt[0] + w, pt[1] + h), (204, 40, 142), 2)
@@ -29,8 +28,12 @@ def process_img(original_img):
         # cv2.circle(original_img, (pt[0]+w, pt[1]+h), 5, (0,0,255), -1)
         zoom_screen = np.array(ImageGrab.grab(bbox=(pt[0], pt[1], pt[0]+w, pt[1]+h)))
         zoom_screen = cv2.resize(zoom_screen, (w*5, h*5))
+        zoom_screen_grey = cv2.cvtColor(zoom_screen, cv2.COLOR_BGR2GRAY)
+        (thresh, BnW_img) = cv2.threshold(zoom_screen_grey, 127, 255, cv2.THRESH_BINARY)
+        text = pytesseract.image_to_string(BnW_img, lang = 'eng')
+        cv2.putText(original_img, str(text), (pt[0]+w, pt[1]+h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        print(text)
 
-    # cv2.putText(original_img, str(text), (0,130), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 4)
     processed_imgs = [grey_img, BnW_img, zoom_screen]
     return processed_imgs
 
@@ -42,7 +45,7 @@ while True:
     screen2 = process_img(screen)
 
     # Подсчет ФПС
-    # print('FPS: {}'.format(1 / (time.time()-last_time) ))
+    print('FPS: {}'.format(1 / (time.time()-last_time) ))
     last_time = time.time()
 
     cv2.imshow('Default screen', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
