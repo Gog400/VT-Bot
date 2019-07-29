e5d0f40f400ac4d2905d2c014b163b7bf01501f7
Vladimir <kirill_kudryavtsevlive@mail.ru>
2019-07-29 14:32:09 +0300

    2

---
+++ test.py
@@ -0,0 +1,8 @@
+    #import ctypes
+
+    #class POINT(ctypes.Structure):
+        #_fields_ = [("x", ctypes.c_ulong), ("y", ctypes.c_ulong)]
+
+    #point = POINT()
+    #ctypes.windll.user32.GetCursorPos(ctypes.pointer(point))
+    #print (point.x, point.y)
